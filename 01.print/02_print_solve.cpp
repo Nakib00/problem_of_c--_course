94f79c73d5ff04274bc7028b98dc267829ea14ad
Md Nakibul islam <67865591+Nakib00@users.noreply.github.com>
2021-10-07 12:21:58 +0600

    print problem up

---
+++ 01.print/02_print_solve.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Find Size of fundamental data types :"<<endl;
+cout<<"------------------------------------------"<<endl;
+cout<<"The sizeof(char) is : 1 bytes"<<endl;
+cout<<"The sizeof(short) is : 2 bytes"<<endl;
+cout<<"The sizeof(int) is : 4 bytes"<<endl;
+cout<<"The sizeof(long) is : 8 bytes"<<endl;
+cout<<"The sizeof(long long) is : 8 bytes"<<endl;
+cout<<"The sizeof(float) is : 4 bytes"<<endl;
+cout<<"The sizeof(double) is : 8 bytes"<<endl;
+cout<<"The sizeof(long double) is : 16 bytes"<<endl;
+cout<<"The sizeof(bool) is : 1 bytes"<<endl;
+return 0;
+}
